c5fece80b2288bf78143fe1b7a7a5ccd5fa1c7d6
Francis Coteur <fcoteur@hotmail.com>
2019-04-26 21:46:05 +0200

    sanitise all controllers

--- backend/controllers/bookmarkController.js
+++ backend/controllers/bookmarkController.js
@@ -1,69 +1,122 @@
 var User = require('../models/user')
+const { validationResult } = require('express-validator/check');
+const { body, param } = require('express-validator/check');
 
-exports.list = function(req, res, next) {
-  const { userId } = req.params;
-
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      res.send({ title: 'Bookmark List', bookmarks: user.bookmarks });
+exports.list = [
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  });
-};
-
-exports.new = function(req, res, next) {
-  const { userId } = req.params;
-  const { name, url } = req.body;
-
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      user.bookmarks.push({ name, url })
-      user.save()
-      res.send({ title: 'New Bookmark', bookmarks: user.bookmarks });
-    }
-  });
-};
-
-exports.update = function(req, res, next) {
-  const { userId } = req.params;
-  const { bookmarkId, name, url } = req.body;
-
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      const indexBookmark = user.bookmarks.findIndex(e => e._id == bookmarkId) 
-      if (indexBookmark !== -1) {
-        user.bookmarks[indexBookmark].name = name
-        user.bookmarks[indexBookmark].url = url
-        user.save()
-        res.send({ title: 'Bookmark updated', bookmarks: user.bookmarks });
+    const { userId } = req.params;
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
       } else {
-        const err = new Error('id of the bookmark is not found in the user profile')
-        next(err)
-      } 
-    }
-  });
-};
+        res.send({ title: 'Bookmark List', bookmarks: user.bookmarks });
+      }
+    });
+  }
+];
 
-exports.delete = function(req, res, next) {
-  const { userId } = req.params;
-  const { bookmarkId } = req.body;
+exports.new = [
+  body('name')
+    .not().isEmpty()
+    .trim()
+    .escape(),
+  body('url')
+    .not().isEmpty()
+    .isURL(),
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+    const { userId } = req.params;
+    const { name, url } = req.body;
 
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      user.bookmarks = user.bookmarks.filter(e => e._id != bookmarkId);
-      user.save();
-      res.send({ title: 'Deleted Bookmark', bookmarks: user.bookmarks });
-    }
-  });
-};
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
+      } else {
+        user.bookmarks.push({ name, url })
+        user.save()
+        res.send({ title: 'New Bookmark', bookmarks: user.bookmarks });
+      }
+    });
+  }
+];
+
+exports.update = [
+  body('bookmarkId')
+    .not().isEmpty(),
+  body('name')
+    .not().isEmpty()
+    .trim()
+    .escape(),
+  body('url')
+    .not().isEmpty()
+    .isURL(),
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+    const { userId } = req.params;
+    const { bookmarkId, name, url } = req.body;
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
+      } else {
+        const indexBookmark = user.bookmarks.findIndex(e => e._id == bookmarkId) 
+        if (indexBookmark !== -1) {
+          user.bookmarks[indexBookmark].name = name
+          user.bookmarks[indexBookmark].url = url
+          user.save()
+          res.send({ title: 'Bookmark updated', bookmarks: user.bookmarks });
+        } else {
+          const err = new Error('id of the bookmark is not found in the user profile')
+          next(err)
+        } 
+      }
+    });
+  }
+];
+
+exports.delete = [
+  body('bookmarkId')
+    .not().isEmpty(),
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+    const { userId } = req.params;
+    const { bookmarkId } = req.body;
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
+      } else {
+        user.bookmarks = user.bookmarks.filter(e => e._id != bookmarkId);
+        user.save();
+        res.send({ title: 'Deleted Bookmark', bookmarks: user.bookmarks });
+      }
+    });
+  }
+];
--- backend/controllers/locationController.js
+++ backend/controllers/locationController.js
@@ -1,69 +1,123 @@
 var User = require('../models/user')
+const { validationResult } = require('express-validator/check');
+const { body, param } = require('express-validator/check');
 
-exports.list = function(req, res, next) {
-  const { userId } = req.params;
-
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      res.send({ title: 'Location List', location: user.locations });
+exports.list = [
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  });
-};
-
-exports.new = function(req, res, next) {
-  const { userId } = req.params;
-  const { city, countryCode } = req.body;
-
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      user.locations.push({ city, countryCode })
-      user.save()
-      res.send({ title: 'New Location', location: user.locations });
-    }
-  });
-};
-
-exports.update = function(req, res, next) {
-  const { userId } = req.params;
-  const { locationId, city, countryCode } = req.body;
-
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      const indexLocation = user.locations.findIndex(e => e._id == locationId) 
-      if (indexLocation !== -1) {
-        user.locations[indexLocation].city = city
-        user.locations[indexLocation].countryCode = countryCode
-        user.save()
-        res.send({ title: 'Location updated', location: user.locations });
+    const { userId } = req.params;
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
       } else {
-        const err = new Error('id of the location is not found in the user profile')
-        next(err)
-      } 
-    }
-  });
-};
+        res.send({ title: 'Location List', location: user.locations });
+      }
+    });
+  }
+];
 
-exports.delete = function(req, res, next) {
-  const { userId } = req.params;
-  const { locationId } = req.body;
-
-  User.findById(userId)
-  .exec(function (err, user) {
-    if (err) { 
-      next(err);
-    } else {
-      user.locations = user.locations.filter(e => e._id != locationId);
-      user.save();
-      res.send({ title: 'Deleted Location', location: user.locations });
+exports.new = [
+  body('city')
+    .not().isEmpty()
+    .trim()
+    .escape(),
+  body('countryCode')
+    .not().isEmpty()
+    .trim()
+    .escape(),
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  });
-};
+    const { userId } = req.params;
+    const { city, countryCode } = req.body;
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
+      } else {
+        user.locations.push({ city, countryCode })
+        user.save()
+        res.send({ title: 'New Location', location: user.locations });
+      }
+    });
+  }
+];
+
+exports.update = [
+  body('locationId')
+    .not().isEmpty(),
+  body('city')
+    .not().isEmpty()
+    .trim()
+    .escape(),
+  body('countryCode')
+    .not().isEmpty()
+    .trim()
+    .escape(),
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const { userId } = req.params;
+    const { locationId, city, countryCode } = req.body;
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
+      } else {
+        const indexLocation = user.locations.findIndex(e => e._id == locationId) 
+        if (indexLocation !== -1) {
+          user.locations[indexLocation].city = city
+          user.locations[indexLocation].countryCode = countryCode
+          user.save()
+          res.send({ title: 'Location updated', location: user.locations });
+        } else {
+          const err = new Error('id of the location is not found in the user profile')
+          next(err)
+        } 
+      }
+    });
+  }
+];
+
+exports.delete = [
+  body('locationId')
+    .not().isEmpty(),
+  param('userId')
+    .not().isEmpty()
+    .trim(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const { userId } = req.params;
+    const { locationId } = req.body;
+    User.findById(userId)
+    .exec(function (err, user) {
+      if (err) { 
+        next(err);
+      } else {
+        user.locations = user.locations.filter(e => e._id != locationId);
+        user.save();
+        res.send({ title: 'Deleted Location', location: user.locations });
+      }
+    });
+  }
+];
